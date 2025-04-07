20ed23270f97fa09248bc4c62c2a262760161081
kimchanho <kkch9805@naver.com>
2025-04-07 12:16:22 +0900

    404 오류등록

--- pybo/__init__.py
+++ pybo/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import MetaData
@@ -16,6 +16,8 @@ naming_convention = {
 db = SQLAlchemy(metadata=MetaData(naming_convention=naming_convention))
 migrate = Migrate()
 
+def page_not_found(e):
+    return render_template('404.html'), 404
 
 def create_app():
     app = Flask(__name__)
@@ -35,7 +37,8 @@ def create_app():
     app.register_blueprint(question_views.bp)
     app.register_blueprint(answer_views.bp)
     app.register_blueprint(auth_views.bp)
-    
+    # 오류페이지
+    app.register_error_handler(404, page_not_found)
     from .filter import format_datetime
     app.jinja_env.filters['datetime'] = format_datetime
     
